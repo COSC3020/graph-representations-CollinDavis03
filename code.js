f246ac86141f7464b2a6a2b5019179c30a93091f
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-10-24 17:04:21 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,16 +1,16 @@
 function convertToAdjList(adjMatrix) {
-    //Initialize an empty adjacent list
+//Initialize an empty adjacent list
     var adjacentList = []   
-    //Iterate each row of the adjacent matrix 
+//Iterate each row of the adjacent matrix 
     for (r = 0; r < adjMatrix.length; r++) {
-        //make a list to hold the neighbors
+//make a list to hold the neighbors
         var inner = [] 
-        //iterate over the columns of the current row
+//iterate over the columns of the current row
         for (c = 0; c < adjMatrix[r].length; c++) {
             if (adjMatrix[r][c] == 1) 
                 inner.push(c)
         }
-        //add list of the neighbors to the inner
+//add list of the neighbors to the inner
         adjacentList.push(inner) 
     }
     return adjacentList;
