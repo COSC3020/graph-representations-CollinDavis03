afb7079d177ff417fecede275fe15927f8c8ed50
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-10-24 11:56:09 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
 function convertToAdjList(adjMatrix) {
+    //Initialize an adj list 
+    var adjlist = []   
+    //have some sort of inner list 
+    //need to fill adj list
     return adjMatrix;
 }
